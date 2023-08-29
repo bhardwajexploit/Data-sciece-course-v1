8eb141abefcf3d580d8ae21983f06ac3f96331b7
bhardwajexploit <bhardwajayush3222@gmail.com>
2023-08-29 17:18:18 +0530

    Computer_vision

--- Computer_vision/dual_cam.py
+++ Computer_vision/dual_cam.py
@@ -3,31 +3,26 @@ import numpy as np
 
 cam1 = cv2.VideoCapture(0)
 cam2 = cv2.VideoCapture(1)
-cv2.namedWindow('cameras')
+cv2.namedWindow('cameras') # required
 cv2.createTrackbar('minimum', 'cameras', 0, 255, lambda x: None)
 cv2.createTrackbar('maximum', 'cameras', 0, 255, lambda x: None)
-
+bs = cv2.createBackgroundSubtractorKNN()
 while True:
     st1, img1 = cam1.read()
     st2, img2 = cam2.read()
+    out = np.hstack((img1, img2))
+    # edge detection
+    min = cv2.getTrackbarPos('minimum', 'cameras')
+    max = cv2.getTrackbarPos('maximum', 'cameras')
+    outline = cv2.Canny(out, min, max, 3)
+    inverted = cv2.bitwise_not(outline)
+    cv2.imshow("cameras", inverted)
+    # background subtraction
+    mask = bs.apply(img2)
+    fg = cv2.bitwise_and(img2, img2, mask=mask)
+    cv2.imshow("mask", mask)
+    cv2.imshow("fg", fg)
 
-    if not st1 or not st2:
-        print("Error reading frames from one or both cameras.")
-        break
-
-    # Resize the frames to have the same dimensions
-    img2_resized = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
-
-    # Horizontally stack the resized frames
-    out = np.hstack((img1, img2_resized))
-
-    min_val = cv2.getTrackbarPos('minimum', 'cameras')
-    max_val = cv2.getTrackbarPos('maximum', 'cameras')
-    outline = cv2.Canny(out, min_val, max_val, 3)  # Apply Canny edge detection
-    cv2.imshow('cameras', outline)
-
-    cv2.imshow("Camera 1", img1)
-    cv2.imshow("Camera 2", img2_resized)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
